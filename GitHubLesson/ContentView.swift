80b51fb2f68a82364a882ba67a223453d7296b90
Marcelo Aguiar <marcelo.aguiar@gmail.com>
2022-02-22 06:54:13 -0800

    Initial Commit

---
+++ GitHubLesson/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubLesson
+//
+//  Created by Cássia Andrade on 2/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubLesson/GitHubLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubLessonApp.swift
+//  GitHubLesson
+//
+//  Created by Cássia Andrade on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
